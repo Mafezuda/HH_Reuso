8285f9c7aa4196dd5dff030a63f2456b525db481
Maria Fernanda <162508333+Mafezuda@users.noreply.github.com>
2025-09-25 14:38:25 -0700

    Update MemoriaPacienteRepositorio.java

--- MemoriaPacienteRepositorio.java
+++ MemoriaPacienteRepositorio.java
@@ -4,9 +4,30 @@ import modelo.Paciente;
 import java.util.*;
 
 public class MemoriaPacienteRepositorio implements PacienteRepositorio {
+    private static MemoriaPacienteRepositorio instancia;
     private Map<String, Paciente> armazenamento = new HashMap<>();
 
-    public void salvar(Paciente paciente) { armazenamento.put(paciente.getId(), paciente); }
-    public Paciente buscarPorId(String id) { return armazenamento.get(id); }
-    public List<Paciente> buscarTodos() { return new ArrayList<>(armazenamento.values()); }
+    private MemoriaPacienteRepositorio() {}
+
+    public static MemoriaPacienteRepositorio getInstancia() {
+        if (instancia == null) {
+            instancia = new MemoriaPacienteRepositorio();
+        }
+        return instancia;
+    }
+
+    @Override
+    public void salvar(Paciente paciente) { 
+        armazenamento.put(paciente.getId(), paciente); 
+    }
+
+    @Override
+    public Paciente buscarPorId(String id) { 
+        return armazenamento.get(id); 
+    }
+
+    @Override
+    public List<Paciente> buscarTodos() { 
+        return new ArrayList<>(armazenamento.values()); 
+    }
 }
